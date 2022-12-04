f36eae9e61f62ca5432bb66dd71b8d1b10b3c954
Christian Kruse <cjk@defunct.ch>
2022-12-04 08:30:20 +0100

    use an enum for units identifier

--- entity/src/ingredient_units.rs
+++ entity/src/ingredient_units.rs
@@ -4,6 +4,21 @@ use async_graphql::*;
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter, DeriveActiveEnum, Serialize, Deserialize, Enum)]
+#[sea_orm(rs_type = "String", db_type = "String(Some(2))")]
+pub enum Units {
+    #[sea_orm(string_value = "pcs")]
+    PCS,
+    #[sea_orm(string_value = "tbsp")]
+    TBSP,
+    #[sea_orm(string_value = "tsp")]
+    TSP,
+    #[sea_orm(string_value = "skosh")]
+    SKOSH,
+    #[sea_orm(string_value = "pinch")]
+    PINCH,
+}
+
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize, SimpleObject)]
 #[sea_orm(table_name = "ingredient_units")]
 #[graphql(concrete(name = "IngredientUnit", params()))]
@@ -11,7 +26,7 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i64,
     pub ingredient_id: i64,
-    pub identifier: String,
+    pub identifier: Units,
     pub base_value: f64,
     pub inserted_at: DateTime,
     pub updated_at: DateTime,
