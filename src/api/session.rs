22fbf7cda3c14fecb63f9bb8755d3326eb8b5c39
Christian Kruse <cjk@defunct.ch>
2025-08-31 09:00:43 +0200

    also respond with token in refresh & login mutations

--- src/api/session.rs
+++ src/api/session.rs
@@ -2,53 +2,63 @@ use ::http::header::SET_COOKIE;
 use async_graphql::*;
 use jwt_simple::prelude::*;
 
-use crate::{AppState, users::authenticate_user};
+use crate::AppState;
+use crate::users::authenticate_user;
 
 #[derive(Default)]
 pub struct SessionMutations;
 
+#[derive(Clone, Debug, SimpleObject)]
+struct AuthResponse {
+    user: entity::users::Model,
+    token: String,
+}
+
 #[Object]
 impl SessionMutations {
-    async fn login(&self, ctx: &Context<'_>, email: String, password: String) -> Result<entity::users::Model> {
+    async fn login(&self, ctx: &Context<'_>, email: String, password: String) -> Result<AuthResponse> {
         let state = ctx.data::<AppState>()?;
 
-        if let Some(user) = authenticate_user(email, password, &state.conn).await {
-            let claims = Claims::create(Duration::from_days(30))
-                .with_issuer("Recipes")
-                .with_subject(user.id.to_string());
+        let Some(user) = authenticate_user(email, password, &state.conn).await else {
+            return Err("Invalid credentials".into());
+        };
 
-            let token = state.token_key.authenticate(claims)?;
+        let claims = Claims::create(Duration::from_days(30))
+            .with_issuer("Recipes")
+            .with_subject(user.id.to_string());
 
-            #[cfg(not(debug_assertions))]
-            ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly; Secure"));
+        let token = state.token_key.authenticate(claims)?;
 
-            #[cfg(debug_assertions)]
-            ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly"));
+        #[cfg(not(debug_assertions))]
+        ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly; Secure"));
 
-            Ok(user)
-        } else {
-            Err("Invalid credentials".into())
-        }
+        #[cfg(debug_assertions)]
+        ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly"));
+
+        Ok(AuthResponse { user, token })
     }
 
-    async fn refresh(&self, ctx: &Context<'_>) -> Result<entity::users::Model> {
+    async fn refresh(&self, ctx: &Context<'_>) -> Result<AuthResponse> {
         let state = ctx.data::<AppState>()?;
 
-        if let Some(user) = ctx.data::<Option<entity::users::Model>>()? {
-            let claims = Claims::create(Duration::from_days(30))
-                .with_issuer("Recipes")
-                .with_subject(user.id.to_string());
-            let token = state.token_key.authenticate(claims)?;
+        let Some(user) = ctx.data::<Option<entity::users::Model>>()? else {
+            return Err("Invalid credentials".into());
+        };
 
-            #[cfg(not(debug_assertions))]
-            ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly; Secure"));
+        let claims = Claims::create(Duration::from_days(30))
+            .with_issuer("Recipes")
+            .with_subject(user.id.to_string());
+        let token = state.token_key.authenticate(claims)?;
 
-            #[cfg(debug_assertions)]
-            ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly"));
+        #[cfg(not(debug_assertions))]
+        ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly; Secure"));
 
-            Ok(user.clone())
-        } else {
-            Err("Invalid credentials".into())
-        }
+        #[cfg(debug_assertions)]
+        ctx.append_http_header(SET_COOKIE, format!("recipes_auth={token}; Path=/; HttpOnly"));
+
+        Ok(AuthResponse {
+            user: user.clone(),
+            token,
+        })
     }
 }
